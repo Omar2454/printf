5aeea4dde83e589956b23f27937f462908b25653
Omar Ahmed <73648173+Omar2454@users.noreply.github.com>
2023-11-12 01:05:12 +0200

    Update main.h

--- main.h
+++ main.h
@@ -1,14 +1,12 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <stdarg.h> // For using variable argument lists
-#include <stddef.h> // For size_t, NULL, etc.
+#include <stdarg.h> 
+#include <stddef.h> 
 
-// Prototype for the custom _printf function
+
 int _printf(const char *format, ...);
 
-// Add prototypes for any additional helper functions you've written for your _printf function.
-// For example, if you have a function to handle printing integers, add its prototype here.
-// void print_int(int n);
 
-#endif // MAIN_H
+
+#endif 
