a8b26c01d6e9dd95f236c499648f1204eab1e520
Omar Ahmed <73648173+Omar2454@users.noreply.github.com>
2023-11-12 03:24:18 +0200

    Update main.h

--- main.h
+++ main.h
@@ -1,25 +1,14 @@
-#ifndef MAIN
-#define MAIN
+#ifndef MAIN_H
+#define MAIN_H
 
 #include <stdarg.h>
-int _putchar(char c);
-int _printf(const char *format, ...);
-int print_char(va_list c);
-int print_string(va_list s);
-int print_int(va_list i);
-int print_dec(va_list d);
-int print_rev(va_list r);
-int print_bin(va_list b);
-int print_unsig(va_list u);
-int print_octal(va_list o);
-int print_x(va_list x);
-int print_X(va_list X);
-int print_rot13(va_list R);
+#include <stdio.h>
+#include <limits.h>
+#include <stdint.h>
 
-typedef struct code_format
-{
-	char *sc;
-	int (*f)(va_list);
-} code_f;
+int _printf(const char* format, ...);
+void print_unsigned(unsigned int n, int base, int is_uppercase, int* count);
+void print_pointer(void* p, int* count);
+void print_int(int n, int* count);
 
-#endif 
+#endif // MAIN_H
