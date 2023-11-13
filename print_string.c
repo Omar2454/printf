dbd9b2577046679e68ca4e610ae22ff9bff4a138
zscbana <zscbana@gmail.com>
2023-11-13 23:40:41 +0200

    correction of task 5

--- print_string.c
+++ print_string.c
@@ -1,6 +1,5 @@
 #include "main.h"
 #include <stdio.h>
-#include <stdlib.h>
 
 /**
  * print_string - A function that prints a string to the standard output.
@@ -12,15 +11,33 @@
 int print_string(va_list s)
 {
 	char *my_string;
-	int  i = 0;
+	int i = 0, count = 0;
 
 	my_string = va_arg(s, char *);
 	if (my_string == NULL)
 		my_string = "(null)";
+
 	while (my_string[i])
 	{
-		_putchar(my_string[i]);
+		// Check if the character is non-printable
+		if (my_string[i] < 32 || my_string[i] >= 127)
+		{
+			// Print non-printable characters in the required format
+			_putchar('\\');
+			_putchar('x');
+			// Print the ASCII code value in hexadecimal (always 2 characters)
+			if (my_string[i] < 16)
+				_putchar('0');
+			// Use the _putchar function to print the hexadecimal value
+			count += _putchar_hex(my_string[i]);
+		}
+		else
+		{
+			// Print printable characters using _putchar
+			_putchar(my_string[i]);
+			count++;
+		}
 		i++;
 	}
-	return (i);
+	return count;
 }
